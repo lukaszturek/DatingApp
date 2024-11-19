024c356c0684bcc716d2eac61a93c112e645530d
Łukasz Turek <lukasz.turek@simpleformat.pl>
2024-11-19 17:57:52 +0100

    End of section 4

---
+++ API/Program.cs
@@ -0,0 +1,18 @@
+using API.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
